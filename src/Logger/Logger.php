b522f265c86818b299e7f4b1ebe756d914a58e7b
内裤叔叔 <294953530@qq.com>
2020-01-15 10:57:16 +0800

    修复事件处理机制

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -108,8 +108,7 @@ class Logger implements LoggerInterface {
             return;
         }
         $message = static::interpolate($message, $context);
-        list($msec, $sec) = explode(' ', microtime());
-        $time   = date('Y-m-d H:i:s', $sec) . '.' . intval($msec * 1000);
+        $time   = date('Y-m-d H:i:s', time());
         $pid    = getmypid();
         $header = "[{$level}] {$time} <{$pid}>";
         switch ($level) { // 渲染颜色
