a417d6b6a582243c944db25404a2afe06aae3d95
内裤叔叔 <294953530@qq.com>
2020-01-19 16:54:48 +0800

    完善路由配置

--- src/Http/Router/Router.php
+++ src/Http/Router/Router.php
@@ -21,10 +21,40 @@ class Router extends AbstractRouter{
      */
     public function initialize(RouteCollector $routeCollector) {
         $routers    = Core::getInstance()->getRouters();
-        print_r($routers);
         if($routers){
             //开始进行路由初始化了
-
+            foreach ($routers as $routerName => $routerConfig){
+                //先看是不是路由组，如果是路由组，进入路由组切割
+                $groups     = $routerConfig['groups'] ?? [];
+                if($groups) {
+                    foreach ($groups as $name => $group){
+                        $this->addRoute($name, $groups);
+                    }
+                }else {
+                    $this->addRoute($routerName, $routerConfig);
+                }
+            }
         }
+    }
+
+    /**
+     * 添加路由
+     *
+     * @param string $router
+     * @param array  $config
+     */
+    public function addRoute(string $router, array $config = []){
+        if(empty($config)){
+            return;
+        }
+        $method = $config['method'] ?? RouterMethod::GET;
+        // 如果有handle 就使用handle，如果
+        $handle = "";
+        if(isset($config['handle'])) {
+            $handle     = $config['handle'];
+        } else {
+            $handle     = $config['class'];
+        }
+        $this->getRouteCollector()->addRoute($method, $router, $handle);
     }
 }
